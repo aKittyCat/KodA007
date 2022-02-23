0d36f1e86085df702a4cb3d8e552f92968c76ede
Alison <alisonserafim@gmail.com>
2022-02-23 17:47:39 -0300

    innitial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ragnarok-mvp-timer
+This page was developed in order to allow you to save your MvP's time in an easy way.
+
+Release the audio permission in your browser to receive a notification when the MvP is born.
+
+It's very easy to use, just select the mvp you just killed, and click add
+
+Optionally you can also enter the coordinates and time of death, if you don't enter the mvp is configured without tomb and the time of death becomes the current one. If you enter the time, the chosen time will be set to time of death.
+
+Credits for images to Ragnaplace, where they are being pulled.
+
+Version 1.0
+
+Based on: https://www.ragnarokmvptimer.com/
---
+++ assets/js/script.js
@@ -0,0 +1,222 @@
+// MVP List
+let mvpJSON = '[{"id":1150,"name":"Flor do Luar","respawn":[{"mapname":"gld_dun01","amount":1,"respawnTime":28800000},{"mapname":"pay_dun04","amount":1,"respawnTime":3600000}]},{"id":1312,"name":"General Tartaruga","respawn":[{"mapname":"tur_dun04","amount":1,"respawnTime":3600000}]},{"id":1583,"name":"Tao Gunka","respawn":[{"mapname":"beach_dun","amount":1,"respawnTime":18000000}]},{"id":1252,"name":"Hatii","respawn":[{"mapname":"xmas_fild01","amount":1,"respawnTime":7200000}]},{"id":1147,"name":"Maya","respawn":[{"mapname":"anthell02","amount":1,"respawnTime":7200000},{"mapname":"gld_dun02_2","amount":1,"respawnTime":28800000}]},{"id":1159,"name":"Freeoni","respawn":[{"mapname":"moc_fild17","amount":1,"respawnTime":7200000}]},{"id":1389,"name":"Drácula","respawn":[{"mapname":"gef_dun01","amount":1,"respawnTime":3600000}]},{"id":2249,"name":"Pyuriel Furiosa","respawn":[{"mapname":"gld2_prt","amount":1,"respawnTime":28800000}]},{"id":1623,"name":"RSX-0806","respawn":[{"mapname":"ein_dun02","amount":1,"respawnTime":7500000}]},{"id":1492,"name":"Samurai Encarnado","respawn":[{"mapname":"ama_dun03","amount":1,"respawnTime":5460000}]},{"id":1087,"name":"Orc Herói","respawn":[{"mapname":"gef_fild03","amount":1,"respawnTime":3600000}]},{"id":1115,"name":"Eddga","respawn":[{"mapname":"gld_dun01_2","amount":1,"respawnTime":28800000},{"mapname":"pay_fild10","amount":1,"respawnTime":7200000}]},{"id":1046,"name":"Doppelganger","respawn":[{"mapname":"gef_dun02","amount":1,"respawnTime":7200000},{"mapname":"gld_dun04","amount":1,"respawnTime":28800000}]},{"id":1373,"name":"Senhor dos Mortos","respawn":[{"mapname":"niflheim","amount":1,"respawnTime":7980000}]},{"id":1685,"name":"Vesper","respawn":[{"mapname":"jupe_core","amount":1,"respawnTime":7200000}]},{"id":1871,"name":"Bispo Decadente","respawn":[{"mapname":"abbey02","amount":1,"respawnTime":7200000}]},{"id":1418,"name":"Serpente Suprema","respawn":[{"mapname":"gon_dun03","amount":1,"respawnTime":5650000}]},{"id":1272,"name":"Senhor das Trevas","respawn":[{"mapname":"gl_chyard","amount":1,"respawnTime":3600000},{"mapname":"gl_chyard_","amount":1,"respawnTime":3600000},{"mapname":"gld_dun04_2","amount":1,"respawnTime":28800000}]},{"id":1190,"name":"Senhor dos Orcs","respawn":[{"mapname":"gef_fild10","amount":1,"respawnTime":7200000}]},{"id":1112,"name":"Drake","respawn":[{"mapname":"treasure02","amount":1,"respawnTime":7200000}]},{"id":2068,"name":"Boitatá ","respawn":[{"mapname":"bra_dun02","amount":1,"respawnTime":7200000}]},{"id":2442,"name":"Superaprendiz","respawn":[{"mapname":"teg_dun02","amount":1,"respawnTime":7200000}]},{"id":2483,"name":"Bafomé Amaldiçoado","respawn":[{"mapname":"gl_cas02_","amount":1,"respawnTime":7200000}]},{"id":1086,"name":"Besouro-Ladrão Dourado","respawn":[{"mapname":"prt_sewb4","amount":1,"respawnTime":3600000}]},{"id":1251,"name":"Cavaleiro da Tempestade","respawn":[{"mapname":"xmas_dun02","amount":1,"respawnTime":3600000}]},{"id":2253,"name":"General Daehyun","respawn":[{"mapname":"gld2_pay","amount":1,"respawnTime":28800000}]},{"id":3074,"name":"Vigia do Tempo","respawn":[{"mapname":"c_tower3_","amount":1,"respawnTime":7200000}]},{"id":1688,"name":"Lady Tanee","respawn":[{"mapname":"ayo_dun02","amount":1,"respawnTime":25200000}]},{"id":2441,"name":"Aprendiz","respawn":[{"mapname":"teg_dun01","amount":1,"respawnTime":28800000}]},{"id":1157,"name":"Faraó ","respawn":[{"mapname":"in_sphinx5","amount":1,"respawnTime":3600000}]},{"id":1630,"name":"Lady Branca","respawn":[{"mapname":"lou_dun03","amount":1,"respawnTime":7000000}]},{"id":2255,"name":"Guardião Morto Kades","respawn":[{"mapname":"gld2_gef","amount":1,"respawnTime":28800000}]},{"id":1038,"name":"Osíris","respawn":[{"mapname":"moc_pryd04","amount":1,"respawnTime":3600000}]},{"id":1059,"name":"Abelha-Rainha","respawn":[{"mapname":"gld_dun02","amount":1,"respawnTime":28800000},{"mapname":"mjolnir_04","amount":1,"respawnTime":7200000}]},{"id":1832,"name":"Ifrit","respawn":[{"mapname":"thor_v03","amount":1,"respawnTime":39600000}]},{"id":1785,"name":"Atroce","respawn":[{"mapname":"gld_dun03_2","amount":1,"respawnTime":28800000},{"mapname":"ra_fild03","amount":1,"respawnTime":10800000},{"mapname":"ra_fild04","amount":1,"respawnTime":18000000},{"mapname":"ve_fild01","amount":1,"respawnTime":10800000},{"mapname":"ve_fild02","amount":1,"respawnTime":21600000}]},{"id":1751,"name":"Valquíria Randgris","respawn":[{"mapname":"odin_tem03","amount":1,"respawnTime":28800000}]},{"id":2087,"name":"Rainha Scaraba","respawn":[{"mapname":"dic_dun02","amount":1,"respawnTime":7200000}]},{"id":2251,"name":"Gioia","respawn":[{"mapname":"gld2_ald","amount":1,"respawnTime":28800000}]},{"id":1768,"name":"Pesar Noturno","respawn":[{"mapname":"ra_san05","amount":1,"respawnTime":18000000}]},{"id":1885,"name":"Gorynych","respawn":[{"mapname":"mosk_dun03","amount":1,"respawnTime":7200000}]},{"id":1039,"name":"Bafomé ","respawn":[{"mapname":"gld_dun03","amount":1,"respawnTime":28800000},{"mapname":"prt_maze03","amount":1,"respawnTime":7200000}]},{"id":1658,"name":"Espadachim Egnigem","respawn":[{"mapname":"lhz_dun02","amount":1,"respawnTime":7200000}]},{"id":3633,"name":"Quimera Venenosa","respawn":[{"mapname":"slabw01","amount":1,"respawnTime":5000}]},{"id":2202,"name":"Kraken","respawn":[{"mapname":"iz_dun05","amount":1,"respawnTime":7200000}]},{"id":1719,"name":"Detardeurus","respawn":[{"mapname":"abyss_03","amount":1,"respawnTime":10800000}]},{"id":2156,"name":"Leak","respawn":[{"mapname":"dew_dun01","amount":1,"respawnTime":7200000}]},{"id":1511,"name":"Amon Ra","respawn":[{"mapname":"moc_pryd06","amount":1,"respawnTime":3600000}]},{"id":2362,"name":"Amon Ra Pesadelo","respawn":[{"mapname":"moc_prydn2","amount":1,"respawnTime":3600000}]},{"id":2165,"name":"Rainha Scaraba","respawn":[{"mapname":"dic_dun03","amount":1,"respawnTime":7200000}]},{"id":1917,"name":"Morroc Ferido","respawn":[{"mapname":"moc_fild22","amount":1,"respawnTime":43200000}]},{"id":1734,"name":"Kiel-D-01","respawn":[{"mapname":"kh_dun02","amount":1,"respawnTime":7200000}]}]';
+let mvpList = JSON.parse(mvpJSON);
+
+mvpList.sort(function(a, b) {
+    a = a.name.toLowerCase();
+    b = b.name.toLowerCase();
+    return a > b ? 1 : -1;
+});
+
+// event Handlers
+let select = document.getElementById('mvp-list');
+select.onchange = updateMapList;
+let btnAddMVP = document.getElementById('button-add-mvp');
+btnAddMVP.onclick = checkForm;
+let inputHour = document.getElementById('hour');
+inputHour.addEventListener("keyup", event => {
+    if (inputHour.value.length == 2) {
+        if (event.keyCode == 8) {} else {
+            inputHour.value += ":";
+        }
+    }
+});
+
+// timer_list is the variable that hold all timers
+let timer_list = [];
+
+// mvp list input
+function updateMVPList() {
+    mvpList.forEach(monster => {
+        let opt = document.createElement('option');
+        opt.value = monster.id;
+        opt.innerHTML = monster.name;
+        select.appendChild(opt);
+    });
+}
+
+// map list input
+function updateMapList() {
+    let mapList = document.getElementById('mvp-map');
+    mapList.innerHTML = "";
+    let id = parseInt(document.getElementById('mvp-list').value);
+    if (!isNaN(id)) {
+        let mvp = getMVPInfo(id);
+        mvp.respawn.forEach(map => {
+            let opt = document.createElement('option');
+            opt.value = map.respawnTime;
+            opt.innerHTML = map.mapname;
+            mapList.appendChild(opt);
+        });
+    }
+}
+
+// onclick "Add MVP"
+function checkForm() {
+    let id = parseInt(document.getElementById('mvp-list').value);
+    if (!isNaN(id)) {
+        let mvp_data = getMVPInfo(id);
+        let mvp_map = document.getElementById('mvp-map');
+        let x = document.getElementById('position-x').value;
+        let y = document.getElementById('position-y').value;
+        let hour = getHourForm();
+
+        let mvp = {
+            id: mvp_data.id,
+            name: mvp_data.name,
+            location: mvp_map[mvp_map.selectedIndex].text,
+            location_x: x,
+            location_y: y,
+            respawn: mvp_map[mvp_map.selectedIndex].value,
+            hours: hour[0],
+            minutes: hour[1],
+            timer_id: undefined
+        };
+
+        console.log(mvp);
+        addMvpToTable(mvp);
+    }
+}
+
+function getMVPInfo(id) {
+    return mvpList.find(monster => monster.id == id);
+}
+
+function addMvpToTable(mvp, save = true) {
+    let table = document.getElementById('mvp-table');
+    let row = table.insertRow(-1);
+    let mvp_image = row.insertCell(0);
+    let mvp_name = row.insertCell(1);
+    let mvp_location = row.insertCell(2);
+    let mvp_hour = row.insertCell(3);
+    let mvp_timer = row.insertCell(4);
+    let mvp_actions = row.insertCell(5);
+    mvp_image.innerHTML = '<img src="https://static.ragnaplace.com/db/npc/gif/' + mvp.id + '.gif" alt="' + mvp.name + '" class="mvp-image">';
+    mvp_name.innerHTML = mvp.name;
+    mvp_location.innerHTML = mvp.location;
+    if (mvp.location_x != "" && mvp.location_y != "") mvp_location.innerHTML += " " + mvp.location_x + "," + mvp.location_y;
+    mvp_hour.innerHTML = mvp.hours.pad(2) + ':' + mvp.minutes.pad(2);
+    mvp.timer_id = setCountdown(mvp_timer, mvp.hours, mvp.minutes, mvp.respawn, 0, row);
+    mvp_actions.innerHTML = '<i class="fa-solid fa-rotate" data-toggle="tooltip" data-placement="top" title="Renew" onclick=renewTimer(' + mvp.timer_id + ')></i> <i class="fa-solid fa-xmark" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Delete" onclick=removeTimer(' + mvp.timer_id + ')></i>';
+    enableTooltip();
+    timer_list.push(mvp);
+    if (save) saveLocalStorage();
+}
+
+function getHourForm() {
+    let hour = document.getElementById('hour').value;
+    let date = new Date();
+    if (hour.trim().length && hour.indexOf(":") > 0) {
+        let hours = hour.split(':')[0];
+        var minutes = hour.split(':')[1];
+        if (hours >= 0 && hours <= 23) date.setHours(hours);
+        if (minutes >= 0 && minutes <= 59) date.setMinutes(minutes);
+    }
+    return [date.getHours(), date.getMinutes()];
+}
+
+//  Timer Functions
+
+let timers = [];
+let current_timer = 0;
+
+function setCountdown(element, hours, minutes, respawn = 3600000, timer_id = 0, row) {
+    let date = new Date();
+    date.setHours(hours);
+    date.setMinutes(minutes);
+    let countDownDate = date.getTime() + parseInt(respawn);
+    let timer_index = (timer_id) ? timer_id : current_timer;
+    if (timer_id == 0) current_timer++;
+
+    let timer = {
+        element: element,
+        respawn: respawn,
+        row: row,
+        timer: "",
+        notified: false
+    };
+
+    timers[timer_index] = timer;
+    timers[timer_index].timer = setInterval(function() { countDown(element, countDownDate, timer_index) }, 500);
+    return timer_index;
+}
+
+function countDown(element, countDownDate, timer_index) {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let hours = parseInt(Math.abs((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    let minutes = parseInt(Math.abs((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    let seconds = parseInt(Math.abs((distance % (1000 * 60)) / 1000));
+
+    element.innerHTML = hours.pad(2) + ":" + minutes.pad(2) + ":" + seconds.pad(2);
+
+    if (distance < 0) {
+        if (element.className != "mvp-alive") element.className = "mvp-alive";
+        let percent = Math.abs(distance) / 720000 * 100;
+        if (percent > 100) percent = 100;
+        element.innerHTML += '<br> ' + Math.floor(percent) + '%';
+        if (!timers[timer_index].notified) {
+            timers[timer_index].notified = true;
+            document.getElementById('mvp-sound').play()
+        }
+    } else {
+        if (element.className != "mvp-died") element.className = "mvp-died";
+    }
+}
+
+function renewTimer(timer_id) {
+    let current_time = new Date();
+    clearInterval(timers[timer_id].timer);
+    setCountdown(timers[timer_id].element, current_time.getHours(), current_time.getMinutes(), timers[timer_id].respawn, timer_id, timers[timer_id].row);
+}
+
+function removeTimer(timer_id) {
+    clearInterval(timers[timer_id].timer);
+    removeFromTimerList(timer_id);
+    let row = timers[timer_id].row;
+    row.innerHTML = "";
+}
+
+// Prototype Function 0 Paddding
+Number.prototype.pad = function(size) {
+    let s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
+
+function enableTooltip() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+}
+
+// storage functions
+
+function removeFromTimerList(id) {
+    timer_list.forEach(function(mvp, index, object) {
+        if (mvp.timer_id == id) object.splice(index, 1);
+    });
+    saveLocalStorage();
+}
+
+function saveLocalStorage() {
+    localStorage.setItem('timer_list', JSON.stringify(timer_list));
+}
+
+function loadLocalStorage() {
+    let timers = localStorage.getItem('timer_list');
+    localStorage.setItem("timer_list", "[]");
+
+    if (timers != null) {
+        timers = JSON.parse(timers);
+        timers.forEach(function(mvp, index) {
+            addMvpToTable(mvp, false);
+        });
+        saveLocalStorage();
+    }
+}
+
+updateMVPList();
+updateMapList();
+loadLocalStorage();
